ad08788aff558129e93dfc244b6680c49192424c
smoot25 <smoot1086@gmail.com>
2025-10-27 16:59:01 +0900

    fix: 기타 오류 수정

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 # 환경 변수들을 pydantic BaseSettings로 관리하게 했습니다..
 # .env 파일을 프로젝트 루트에 두면 자동으로 읽어오게 했습니다.
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
