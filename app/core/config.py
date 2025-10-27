753ce3d396fe1e302d9bbdc9b4e05f45144a4b2e
smoot25 <smoot1086@gmail.com>
2025-10-27 17:31:00 +0900

    feature: postgreSQL도 호환

--- app/core/config.py
+++ app/core/config.py
@@ -10,15 +10,23 @@ class Settings(BaseSettings):
 
     # MySQL 연결 문자열
     # TORTOISE MYSQL 설치 후 사용 : pip install tortoise-orm[aiomysql]
+    # TORTOISE postgreSQL 설치 후 사용 : pip install tortoise-orm[asyncpg]
+    #
     DB_USER: str = "root"
     DB_PASSWORD: str = "1234"
     DB_HOST: str = "localhost"
     DB_PORT: str = "3306"
     DB_NAME: str = "diary_db"
 
+    # postgre, mysql 모두 호환
     @property
-    def DATABASE_URL(self) -> str:
-        return f"mysql+aiomysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    def database_url(self) -> str:
+        if self.DB_TYPE == "mysql":
+            return f"mysql+aiomysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        elif self.DB_TYPE == "postgresql":
+            return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        else:
+            raise ValueError("Unsupported DB_TYPE. Choose 'mysql' or 'postgresql'.")
 
 
     # SQLITE 연결
